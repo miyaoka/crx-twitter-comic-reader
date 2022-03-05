ce8d1d8cbe7fc3c7479642f44c93a33726335675
Masaya Kazama <miyan@t-p.jp>
2022-03-05 10:05:06 +0900

    drop alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { resolve } from "path";
 import { defineConfig } from "vite";
 import { crx } from "rollup-plugin-chrome-extension";
 import manifest from "./manifest.config";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
-    },
-  },
   plugins: [crx({ manifest })],
 });
