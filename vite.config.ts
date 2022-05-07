ee90d07c7e3320c2011b7f1d5d4dfcd4bc5f01a9
Masaya Kazama <miyan@t-p.jp>
2022-05-07 19:44:39 +0900

    migrate RPCE

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import { crx } from "rollup-plugin-chrome-extension";
+import { crx } from "@crxjs/vite-plugin";
 import manifest from "./manifest.config";
 
 export default defineConfig({
